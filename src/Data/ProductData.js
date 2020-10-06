83dab11b5011483dbfd6f9c16dc00291c8754bc9
Yvan <16927302+xYvanB@users.noreply.github.com>
2020-10-06 10:53:37 +0200

    [refactor] added folder img to not have to depend about cover img's link anymore

--- src/Data/ProductData.js
+++ src/Data/ProductData.js
@@ -4,7 +4,7 @@ const ProductData = [
         isbn: '88-09-05287-0',
         price: "10",
         description: "Hyperversum",
-        img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.ceciliarandall.it%2Fwp-content%2Fuploads%2F2017%2F01%2Fcop_low_hyperversum.jpg&f=1&nofb=1",
+        img: require('../img/coverHv.jpg'),
         alt: "Hyperversum",
     },
     {
@@ -12,7 +12,7 @@ const ProductData = [
         isbn: '978-88-09-05528-5',
         price: "20",
         description: "Hyperversum - Il falco e il leone",
-        img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdbtitoli.giunti.it%2Fapi%2Fmedia%2Fimage%2FgetImage%3Fcm%3D65879Y%26type%3DP&f=1&nofb=1",
+        img: require('../img/coverHvFalcoLeone.jpg'),
         alt: "Hyperversum - Il falco e il leone",
     },
     {
@@ -20,7 +20,7 @@ const ProductData = [
         isbn: '978-88-09-81878-1',
         price: "20",
         description: "Hyperversum - Il cavaliere del tempo",
-        img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdbtitoli.giunti.it%2Fapi%2Fmedia%2Fimage%2FgetImage%3Fcm%3D65878E%26size%3Dmedium%26type%3DP&f=1&nofb=1",
+        img: require('../img/coverHvCavaliereTempo.jpg'),
         alt: "Hyperversum - Il cavaliere del tempo",
     },
     {
@@ -28,7 +28,7 @@ const ProductData = [
         isbn: '978-88-09-86221-0',
         price: "25",
         description: "Hyperversum Next",
-        img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.CUSflus5892B-cPgH8Q9tQHaLX%26pid%3DApi&f=1",
+        img: require('../img/coverHvNext.jpg'),
         alt: "Hyperversum Next",
     }
 ]
